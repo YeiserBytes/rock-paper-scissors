28489b7b067676d049eec136aa87d31cc9e03e61
yeiserJimenez <yeiser192@gmail.com>
2024-05-27 19:18:54 -0400

    ✅ refactor: Update SVG file structure and remove unused file

--- src/components/ButtonClose.tsx
+++ src/components/ButtonClose.tsx
@@ -1,7 +1,8 @@
 export default function ButtonClose({ onClick }: { onClick: () => void}) {
   return (
     <button onClick={onClick}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path fill="#3B4262" fillRule="evenodd" d="M16.97 0l2.122 2.121-7.425 7.425 7.425 7.425-2.121 2.12-7.425-7.424-7.425 7.425L0 16.97l7.425-7.425L0 2.121 2.121 0l7.425 7.425L16.971 0z" opacity=".25" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path fill="#3B4262" fillRule="evenodd" d="M16.97 0l2.122 2.121-7.425 7.425 7.425 7.425-2.121 2.12-7.425-7.424-7.425 7.425L0 16.97l7.425-7.425L0 2.121 2.121 0l7.425 7.425L16.971 0z" opacity=".25" />
+      </svg>
     </button>
   )
 }
